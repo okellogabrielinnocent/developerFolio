d08464cf444193825a9f4bd4b24ea8698f9b76b1
Okello Gabriel Innocent <okellogabrielinnocent@gmail.com>
2020-08-08 01:15:41 +0300

    Updates

---
+++ precache-manifest.a8a809c9f25ee0c571244f19bfdb23e8.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f323b38f987c9b380760e640e0d2009",
+    "url": "/developerFolio/index.html"
+  },
+  {
+    "revision": "4cd6283fa9fc5509f0a3",
+    "url": "/developerFolio/static/css/3.cffc9e54.chunk.css"
+  },
+  {
+    "revision": "2ac729ebd2bec7790050",
+    "url": "/developerFolio/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "fc2bd92989b012ce1f55",
+    "url": "/developerFolio/static/css/main.5ce078cb.chunk.css"
+  },
+  {
+    "revision": "1219b2a7903f39360351",
+    "url": "/developerFolio/static/js/2.ce5bbc04.chunk.js"
+  },
+  {
+    "revision": "4cd6283fa9fc5509f0a3",
+    "url": "/developerFolio/static/js/3.dd8c4976.chunk.js"
+  },
+  {
+    "revision": "2ac729ebd2bec7790050",
+    "url": "/developerFolio/static/js/4.aba8f46e.chunk.js"
+  },
+  {
+    "revision": "fc2bd92989b012ce1f55",
+    "url": "/developerFolio/static/js/main.e236eda9.chunk.js"
+  },
+  {
+    "revision": "c579a38649e24b6a583c",
+    "url": "/developerFolio/static/js/runtime-main.d5f4598a.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/developerFolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "3d95444963ef812a750c799b161877d2",
+    "url": "/developerFolio/static/media/Andela.3d954449.png"
+  },
+  {
+    "revision": "3ceca25672bd98043466c27645e9645d",
+    "url": "/developerFolio/static/media/GDG.3ceca256.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/developerFolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c4622b063aafecfda32a23b822847ef5",
+    "url": "/developerFolio/static/media/afriadd.c4622b06.png"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/developerFolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/developerFolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/developerFolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "add729c714f5813f759da2cfe2bf1f6b",
+    "url": "/developerFolio/static/media/lynda.add729c7.jpg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/developerFolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "5e97269d37f7269bfd0efd2033c7c5ba",
+    "url": "/developerFolio/static/media/nssf.5e97269d.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/developerFolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/developerFolio/static/media/talksCardBack.0288c5ec.svg"
+  }
+]);
